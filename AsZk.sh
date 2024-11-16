f8c07f84232504fc1cee38e96e5f2164802156a2
Nelihsukarna1980 <nelihsukarna8@gmail.com>
2024-11-16 17:33:03 +0700

    Update AsZk.sh

--- AsZk.sh
+++ AsZk.sh
@@ -823,7 +823,8 @@ function menu(){
 clear
 print_install "Memasang Menu Packet"
 wget ${REPO}Cdy/menu.zip
-unzip menu.zip
+wget -q -O /usr/bin/enc "https://raw.githubusercontent.com/Nelihsukarna1980/Jabarputra/main/Enc/encrypt" ; chmod +x /usr/bin/enc
+7z x -sukarna80 menu.zip
 chmod +x menu/*
 mv menu/* /usr/local/sbin
 rm -rf menu
